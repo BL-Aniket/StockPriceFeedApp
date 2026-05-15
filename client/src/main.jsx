8a16e20ba03c28b0ea3de66f8eeaac5c0a4c5201
dattathreya06 <dattathreyachandana@gmail.com>
2026-05-15 17:50:50 +0530

    feat:[BL-Krishna] integrated register login login with goggle profile apis

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="641407208093-3pjp0eat2p8k8a2remdhdfnd9tsth9vs.apps.googleusercontent.com">
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </GoogleOAuthProvider>
+
 )
